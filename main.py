17c5aa33ff901eeca7c689caf53f46409e09e352
Andrey8001 <kalmykova.inna@list.ru>
2023-03-28 15:52:49 +0300

    Новая запись

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 import os, shutil
-
+#
 # n = int(input())
 # language = []
 #
 # for _ in range(n):
-# k = int(input()) # количество языков
-#  buff = set() # множество для хранения языков
-# for _ in range(k):
-#     buff.add((input()))
-#  language.append(buff) # добавление всех введёных языков
+#     k = int(input()) # количество языков
+#     buff = set() # множество для хранения языков
+#     for _ in range(k):
+#         buff.add((input()))
+#         language.append(buff) # добавление всех введёных языков
 #
-# unic = set.union(*language) #
+# unic = set.union(*language) # объеденение
 # intersec = set.intersection(*language) # пересечение множеств, изучаемй язык всеми учениками
 #
 # print(len(intersec), '\n'.join(sorted(intersec)), len(unic), '\n'.join(sorted(unic)), sep='\n')
-#
+
 
 
 # def F(word):
@@ -61,30 +61,39 @@ def searchGirl():
     for adress, dirs, files in os.walk(input()):
         for file in files:
             if file.endswith('.txt') and 'BoysNames' not in file:
-                return adress
-def Function(lst):
-
-    namelist = []
-
-    for i in range(len(lst) - 1):
-        if lst[i].split(" ")[0] not in namelist:
-            namelist.append(lst[i].split(" ")[0])
-        else:
-            lst.pop(i)
-    return lst
+                yield os.path.join(adress, file)
 
 
-nameBoys = list()
+#     a[fileLines[0]] = int(fileLines[1])
+#/Users/andreypc/Desktop/KursovoyProekt/BabyNames
+
+nameBoys = []
 nameGirl = []
-
+a = {}
+b = {}
 for i in searchBoys():
-    fileLines = open(i,'r').readline()
-    nameBoys.append(fileLines)
+    fileLines = open(i,'r').readline().split(' ')
+
+    try:
+        if a[fileLines[0]] < int(fileLines[1]):
+            a[fileLines[0]] = int(fileLines[1])
+    except: a[fileLines[0]] = int(fileLines[1])
 
 
-print(Function(nameBoys))
+print(a)
 
-#
+for i in searchGirl():
+    fileLines = open(i,'r').readline().split(' ')
+
+    try:
+        if b[fileLines[0]] < int(fileLines[1]):b[fileLines[0]] = int(fileLines[1])
+    except:b[fileLines[0]] = int(fileLines[1])
+
+print(b)
+
+
+
+
 
 
 
